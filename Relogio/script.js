7273e6a61815dc6026780ef5a7ed966b5d5098e6
nathaliadt <nathalia.diastxr@gmail.com>
2022-07-06 19:42:00 -0400

    projeto completo

---
+++ Relogio/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem') 
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerText = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.jpg'
+        document.body.style.background = '#deff8f'
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#ffe258'
+    }else {
+        img.src = 'noite.jpg'
+        document.body.style.background = '#2d2a26'
+    }
+}
+
